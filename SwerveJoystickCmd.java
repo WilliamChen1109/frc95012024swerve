d2a4be9451e06d33c859cd198eb8af0d47f56565
Chen, Wei-Yu <49446811+WilliamChen1109@users.noreply.github.com>
2024-01-19 14:53:26 +0800

    Create SwerveJoystickCmd.java

---
+++ SwerveJoystickCmd.java
@@ -0,0 +1,29 @@
+package frc.robot.commands;
+
+import java.util.function.Supplier;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.subsystems.SwerveSubsystem;
+
+public class SwerveJoystickCmd extends Command{
+    private final SwerveSubsystem swerveSubsystem;
+    private final Supplier<Double> xSpdFunction, ySpdFunction, turningSpdFunction;
+    private final Supplier<Boolean> fieldOrientedFunction;
+    private final SlewRateLimiter xLimiter, yLimiter, turningLimiter;
+
+    public SwerveJoystickCmd(SwerveSubsystem swerveSubsystem, Supplier<Double> xSpdFunction,
+            Supplier<Double> ySpdFunction, Supplier<Double> turningSpdFunction, Supplier<Boolean> fieldOrientedFunction){
+        this.swerveSubsystem = swerveSubsystem;
+        this.xSpdFunction = xSpdFunction;
+        this.ySpdFunction = ySpdFunction;
+        this.turningSpdFunction = turningSpdFunction;
+        this.fieldOrientedFunction = fieldOrientedFunction;
+        this.xLimiter = new SlewRateLimiter(DriveConstants.kTeleDriveMaxAccelerationUnitsPerSecond);
+        this.yLimiter = new SlewRateLimiter(DriveConstants.kTeleDriveMaxAccelerationUnitsPerSecond);
+        this.turningLimiter = new SlewRateLimiter(DriveConstants.kTeleDriveMaxAngularAccelerationUnitsPerSecond);
+        
+        addRequirements(swerveSubsystem);
+    }
+}
